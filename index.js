73c517ed3b09832fb1f0062ec22fdfe39a29c016
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-01-25 15:32:58 +0100

    Redis support

--- index.js
+++ index.js
@@ -20,14 +20,22 @@ if (env == "development") {
     log.level("debug");
 }
 
-var redisClient = redis.createClient(config.redis.port, config.redis.host);
+var redisClient;
+var redisLocation;
+if (config.redis.socket) {
+    redisClient = redis.createClient(config.redis.socket);
+    redisLocation = config.redis.socket;
+} else {
+    redisLocation = config.redis.host + ":" + config.redis.port;
+    redisClient = redis.createClient(config.redis.port, config.redis.host);
+}
 redisClient.on("error", function(err) {
     log.error("Redis error", err);
 });
 
-log.info("Waiting for redis server " + config.redis.host + ":" + config.redis.port + " to start");
+log.info("Waiting for connection to redis server " + redisLocation);
 redisClient.on("ready", function() {
-    log.info("Redis is ready at http://%s:%s", config.redis.host, config.redis.port);
+    log.info("Redis is ready at " + redisLocation);
     var WebSocketServer = require('websocket').server;
     var http = require('http');
 
